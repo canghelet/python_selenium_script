ccccabfc70bd9d03162e481f27cf8ce9f1665c26
Cristina <cristina.anghelet@gmail.com>
2023-05-17 19:52:08 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Hello!
+
+This is my web automation script generated with Python and Selenium.
+1. The script was created to automate browser tasks on "https://carturesti.ro/" website.
+2. Installation:\
+	a) Install Python bindings for Selenium using "pip install selenium";\
+	b) Install Chrome WebDriver from "https://sites.google.com/chromium.org/driver/downloads" webpage;
+3. Usage:\
+	a) Script will be executed by using command "python python_selenium_script.py" from terminal/cmd;\
+	b) When the script runs, it will automatically open Chrome browser, navigate to "https://carturesti.ro/" webpage, maximize the window, find and return all the links of the webpage;\
+	c) Will automatically locate on the homepage the Search bar by ID, will enter "carti" text, perform the search by the given key and return the results on the page;\
+	d) On the same page, will locate relevant books elements by XPATH, open the page for each book and add them to cart;\
+	e) Page will be redirected to the homepage, script will locate and return the length of list with all elements having a specific CLASS_NAME;\
+	f) It will navigate through pages by opening each button from navigation bar, redirecting to: "https://carturesti.ro/info/despre-carturesti-ro", "https://carturesti.ro/librarii", "https://carturesti.ro/blog";
+
+ 	g) Page will be redirected to the homepage and will automatically fill in and submit web form in order to subscribe to the website newsletter;\
+	h) Browser will close.
---
+++ python_selenium_script.py
@@ -0,0 +1,74 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+driver.get('https://carturesti.ro/')
+driver.maximize_window()
+
+# get all the links of the webpage
+links = driver.find_elements(By.XPATH, '//a[@href]')
+for link in links:
+    print(link.get_attribute("innerHTML"))
+
+# locate search bar and perform action by entering carti text and return the results
+searchBar = driver.find_element(By.ID, "search-input")
+searchBar.clear()
+searchBar.send_keys("carti")
+searchBar.send_keys(Keys.RETURN)
+time.sleep(3)
+
+# locate book elements and add them to cart
+driver.find_element(By.XPATH, '//*[@id="coloana-produse"]/div/div/div[1]/prod-grid-box/a/div/img').click()
+driver.find_element(By.XPATH, '//*[@id="cartu-add-to-cart-btn-x"]/a').click()
+driver.back()
+time.sleep(3)
+
+driver.find_element(By.XPATH, '//*[@id="coloana-produse"]/div/div/div[6]/prod-grid-box/a/div/img').click()
+driver.find_element(By.XPATH, '//*[@id="cartu-add-to-cart-btn-x"]/a').click()
+driver.back()
+time.sleep(3)
+
+# return the length of list with all elements having a specific class name
+driver.get('https://carturesti.ro/')
+items = driver.find_elements(By.CLASS_NAME, "ng-scope")
+print(len(items))
+time.sleep(3)
+
+# open https://carturesti.ro/info/despre-carturesti-ro page
+driver.find_element(By.ID, "carturestiDropdown").click()
+driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div/div[2]/md-menu-bar[2]/md-menu-item[1]/ul/li[1]/a').click()
+time.sleep(3)
+
+# open https://carturesti.ro/librarii page
+driver.find_element(By.ID, "carturestiDropdown").click()
+driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div/div[2]/md-menu-bar[2]/md-menu-item[1]/ul/li[2]/a').click()
+time.sleep(3)
+
+# open https://carturesti.ro/blog page
+driver.find_element(By.ID, "carturestiDropdown").click()
+driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div/div[2]/md-menu-bar[2]/md-menu-item[1]/ul/li[3]/a').click()
+time.sleep(3)
+driver.back()
+
+# fill in web form and subscribe to newsletter
+driver.get('https://carturesti.ro/')
+email = driver.find_element(By.XPATH, '//*[@id="mce-EMAIL"]')
+email.send_keys("angheletcristina@yahoo.com")
+time.sleep(3)
+
+first_name = driver.find_element(By.XPATH,  '//*[@id="mce-FNAME"]')
+first_name.send_keys("Anghelet")
+time.sleep(3)
+
+last_name = driver.find_element(By.XPATH, '//*[@id="mce-LNAME"]')
+last_name.send_keys("Cristina")
+time.sleep(3)
+
+subscribe_newsletter = driver.find_element(By.XPATH, '//*[@id="mc-embedded-subscribe"]')
+time.sleep(3)
+subscribe_newsletter.click()
+time.sleep(3)
+driver.close()
